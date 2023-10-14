fa607abb421c4c83987d64f76ee0424c24e75edc
Elijah Nicpon <elija@lawn-10-91-94-15.lawn.gatech.edu>
2023-10-14 00:44:00 -0400

    init backend

---
+++ flashys_backend/src/main/java/com/flashys/flashys_backend/FlashysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flashys.flashys_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashysBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashysBackendApplication.class, args);
+    }
+
+}
---
+++ flashys_backend/src/test/java/com/flashys/flashys_backend/FlashysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashys.flashys_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashysBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
