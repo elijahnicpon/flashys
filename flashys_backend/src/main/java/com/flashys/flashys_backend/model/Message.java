e933a4475929ced7b3c1c28321ad977acc343bfe
egoldin03 <egoldin03@gmail.com>
2023-10-14 11:02:15 -0400

    Yay2

--- flashys_backend/src/main/java/com/flashys/flashys_backend/model/Message.java
+++ flashys_backend/src/main/java/com/flashys/flashys_backend/model/Message.java
@@ -1,5 +1,8 @@
 package com.flashys.flashys_backend.model;
 
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 @Data
 @Document(collection = "message")
 public class Message {
