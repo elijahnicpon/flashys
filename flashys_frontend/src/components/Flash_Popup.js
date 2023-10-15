256b508f8a62102b548737c48b990a872837a0bf
jiwon-lee0420 <jiwonmoohoon@gmail.com>
2023-10-14 22:44:26 -0400

    wrote some more to flash popup

--- flashys_frontend/src/components/Flash_Popup.js
+++ flashys_frontend/src/components/Flash_Popup.js
@@ -1,11 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import './Flash_Popup.css';
 
-const [flashPopup, setFlashPopup] = useState(null);
+const [flashPopup, setFlashPopup] = useState([0]);
+const [flashes, setFlashes] = useState([]);
+const [artists, setArtists] = useState([0]);
+
+useEffect(() => {
+  fetch('http://localhost:8080/flash/all')
+        .then((response) => response.json())
+        .then((data) => setFlashes(data))
+        .catch((error) => console.error('Error fetching data:', error));
+}, []);
+
+useEffect(() => {
+  fetch('http://localhost:8080/artist/all')
+      .then((response) => response.json())
+      .then((data) => setArtist(data))
+      .catch((error) => console.error('Error fetching artist', error))
+})
+
+const closeModal = () => setFlashPopup(null);
+
+console.log(artists);
 
 const Flash_Popup = () => {
   return (
-    <div>Flash_Popup</div>
+    <>
+      <h1>Artist: </h1>
+      <div className="flash-grid">
+        {flashes.map((flash) => (
+          <div key={flash.id} onClick={() => openModal(flash)}>
+            <img src={`data:image/png;base64,${flash.img}`} alt={flash.title} />
+            <h2>{flash.title}</h2>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
