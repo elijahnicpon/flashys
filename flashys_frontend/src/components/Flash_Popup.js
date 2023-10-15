16ee15e6f93d9431af992af39aa148f5a05efb72
jiwon-lee0420 <jiwonmoohoon@gmail.com>
2023-10-14 22:25:40 -0400

    working on flash popup

--- flashys_frontend/src/components/Flash_Popup.js
+++ flashys_frontend/src/components/Flash_Popup.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import Flashes from '../pages/Flashes.js';
+import React, { useState, useEffect } from 'react'
+import './Flash_Popup.css';
 
+const [flashPopup, setFlashPopup] = useState(null);
 
 const Flash_Popup = () => {
   return (
